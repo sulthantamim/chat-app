19a9ee05c134695c209074c434342e916d44c5b0
sulthantamim <tamimsulthan2@gmail.com>
2025-10-28 23:40:15 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public")); // folder untuk file HTML dan JS tampilan
+
+io.on("connection", (socket) => {
+  console.log("Pengguna baru terhubung!");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg); // kirim pesan ke semua pengguna
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Pengguna keluar.");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server berjalan di http://localhost:3000");
+});
