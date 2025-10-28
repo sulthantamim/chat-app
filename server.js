6bff83e6bd0e369998bc4397b25d07d80942e383
sulthantamim <tamimsulthan2@gmail.com>
2025-10-29 00:00:42 +0700

    Add dynamic port for Railway

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server berjalan di http://localhost:3000");
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server berjalan di port ${PORT}`);
 });
